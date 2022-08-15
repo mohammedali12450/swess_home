c82333180552e7b37978f8b40b3f8069a9e0ca5b
Omar_new <omarr.ng19@gmail.com>
2022-08-15 14:56:36 +0300

    - hide advanced search

--- lib/modules/data/models/price_domain.dart
+++ lib/modules/data/models/price_domain.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class PriceDomain {
   int id;
 
@@ -11,13 +13,13 @@ class PriceDomain {
       return isArabic? "غير محدد" : "undefined";
     }
     if (min == "0") {
-      return isArabic ? ("أقل من " + max) : ("less than $max");
+      return isArabic ? ("أقل من " " "+ NumberFormat('###,###,###').format(int.parse(max))) : ("less than" " "+ NumberFormat('###,###,###').format(int.parse(max)));
     }
     if (max == "999999999999999999") {
-      return isArabic ? ("أكثر من " + min) : ("more than $min") ;
+      return isArabic ? ("أكثر من "  " " + NumberFormat('###,###,###').format(int.parse(min))) : ("more than " " "+ NumberFormat('###,###,###').format(int.parse(min))) ;
     }
 
-    return isArabic ? ("بين " + min + " و " + max) : ("between $min and $max");
+    return isArabic ? ("بين "  " " + NumberFormat('###,###,###').format(int.parse(min)) + " و " + NumberFormat('###,###,###').format(int.parse(max))) : ("between " " "+NumberFormat('###,###,###').format(int.parse(min)) +" " + "and" + " "+ NumberFormat('###,###,###').format(int.parse(max)));
   }
 
   factory PriceDomain.fromJson(json) {
@@ -29,4 +31,6 @@ class PriceDomain {
       max: json['max'],
     );
   }
+
+
 }
