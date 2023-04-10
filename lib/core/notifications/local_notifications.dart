84ca7c79760a0fad72453a340a0a9a0920a887e3
NourAldinAlTeilouny <noor.teilouny@gmail.com>
2023-04-10 10:08:47 +0300

    replace IOSInitializationSettings to DarwinInitializationSettings like doc says And refactor code

--- lib/core/notifications/local_notifications.dart
+++ lib/core/notifications/local_notifications.dart
@@ -2,34 +2,25 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 // Flutter local notifications plugin //
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 // Initializing settings //
 //android:
-const AndroidInitializationSettings initializationSettingsAndroid =
-AndroidInitializationSettings('@mipmap/ic_launcher');
+const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  final IOSInitializationSettings initializationSettingsIOS =
-      IOSInitializationSettings(
-          requestAlertPermission: false,
-          requestBadgePermission: false,
-          requestSoundPermission: false,
-          onDidReceiveLocalNotification: (
-            int id,
-            String? title,
-            String? body,
-            String? payload,
-          ) async {
-          
-          });
+final DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings(
+    requestAlertPermission: false,
+    requestBadgePermission: false,
+    requestSoundPermission: false,
+    onDidReceiveLocalNotification: (
+      int id,
+      String? title,
+      String? body,
+      String? payload,
+    ) async {});
 //initializing:
-InitializationSettings initializationSettings =  InitializationSettings(
-  android: initializationSettingsAndroid,iOS: initializationSettingsIOS);
+InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
 // Android Notifications Channel //
-const AndroidNotificationChannel androidNotificationsChannel = AndroidNotificationChannel(
-    'high_importance_channel', 'High Importance Notifications',
-    description: "This Channel is used for importance notifications",
-    importance: Importance.high,
-    playSound: true);
+AndroidNotificationChannel androidNotificationsChannel = const AndroidNotificationChannel('high_importance_channel', 'High Importance Notifications',
+    description: "This Channel is used for importance notifications", importance: Importance.high, playSound: true);
