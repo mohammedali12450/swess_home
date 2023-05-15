4988dc631530db8daae8bbf148d1983e3821a2f3
@maya.skaf <maya.skaf@axtro-soft.com>
2023-05-15 13:42:13 +0300

    fixing the width and height of elevated-button that exists in wonderful alert dialog

--- lib/modules/presentation/widgets/wonderful_alert_dialog.dart
+++ lib/modules/presentation/widgets/wonderful_alert_dialog.dart
@@ -39,57 +39,60 @@ Future<void> showWonderfulAlertDialog(
             Radius.circular(borderRadius ?? 8),
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              title,
-              style: titleTextStyle ?? Theme.of(context).textTheme.bodyText1,
-            ),
-            24.verticalSpace,
-            Text(
-              body,
-              style: bodyTextStyle ??
-                  Theme.of(context).textTheme.bodyText2!.copyWith(
-                    height: 1.8,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                title,
+                style: titleTextStyle ?? Theme.of(context).textTheme.bodyText1,
+              ),
+              24.verticalSpace,
+              Text(
+                body,
+                style: bodyTextStyle ??
+                    Theme.of(context).textTheme.bodyText2!.copyWith(
+                      height: 1.8,
+                    ),
+                maxLines: 50,
+                textAlign: TextAlign.center,
+              ),
+              32.verticalSpace,
+              Wrap(
+                alignment: WrapAlignment.center,
+                direction: Axis.horizontal,
+                spacing: 12.h,
+                runSpacing: 12.w,
+                children: [
+                  (removeDefaultButton)
+                      ? Container()
+                      : ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: Size(88.w, 56.h),
+                        maximumSize: Size(100.w, 56.h),
+                        padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 12.w)),
+                    onPressed: () {
+                      if (onDefaultButtonPressed != null) {
+                        onDefaultButtonPressed();
+                        return;
+                      }
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      defaultButtonContent ?? AppLocalizations.of(context)!.ok,
+                      style: defaultButtonContentStyle ??
+                          Theme.of(context)
+                              .textTheme
+                              .subtitle2!
+                              .copyWith(color: AppColors.white),
+                    ),
                   ),
-              maxLines: 50,
-              textAlign: TextAlign.center,
-            ),
-            32.verticalSpace,
-            Wrap(
-              alignment: WrapAlignment.center,
-              direction: Axis.horizontal,
-              spacing: 12.h,
-              runSpacing: 12.w,
-              children: [
-                (removeDefaultButton)
-                    ? Container()
-                    : ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: Size(88.w, 56.h),
-                      padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 12.w)),
-                  onPressed: () {
-                    if (onDefaultButtonPressed != null) {
-                      onDefaultButtonPressed();
-                      return;
-                    }
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    defaultButtonContent ?? AppLocalizations.of(context)!.ok,
-                    style: defaultButtonContentStyle ??
-                        Theme.of(context)
-                            .textTheme
-                            .subtitle2!
-                            .copyWith(color: AppColors.white),
-                  ),
-                ),
-              ] +
-                  (dialogButtons ?? []),
-            ),
-          ],
+                ] +
+                    (dialogButtons ?? []),
+              ),
+            ],
+          ),
         ),
       ),
     ),
