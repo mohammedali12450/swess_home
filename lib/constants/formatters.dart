4f200b8b487371ad6f6dc0c49404f8d434badf2b
Skaf-Maya <maya98skaf@gmail.com>
2023-06-25 23:22:25 +0300

    enabling the symbols in the password fields

--- lib/constants/formatters.dart
+++ lib/constants/formatters.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
 
 final onlyEnglishLetters =
-    FilteringTextInputFormatter.allow(RegExp('[0-9a-zA-Z]'));
+    // FilteringTextInputFormatter.allow(RegExp('[0-9a-zA-Z]'));
+    FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9!@#\$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]+'));
