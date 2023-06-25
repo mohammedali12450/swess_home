589afb02fa9971ca8f98a9adbd7471d53376626e
Skaf-Maya <maya98skaf@gmail.com>
2023-06-25 18:49:37 +0300

    adding the new formatters

--- lib/constants/formatters.dart
+++ lib/constants/formatters.dart
@@ -2,3 +2,7 @@ import 'package:flutter/services.dart';
 
 final onlyEnglishLetters =
     FilteringTextInputFormatter.allow(RegExp('[0-9a-zA-Z]'));
+
+final only15Numbers = LengthLimitingTextInputFormatter(15);
+
+final onlyNumbers =  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'));
