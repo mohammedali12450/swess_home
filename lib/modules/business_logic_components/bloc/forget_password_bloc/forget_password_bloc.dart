0aed65a7edd31c4a020c942acd54ce1e4846344b
Skaf-Maya <maya98skaf@gmail.com>
2023-07-04 13:25:15 +0300

    handling the messages from api

--- lib/modules/business_logic_components/bloc/forget_password_bloc/forget_password_bloc.dart
+++ lib/modules/business_logic_components/bloc/forget_password_bloc/forget_password_bloc.dart
@@ -2,25 +2,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swesshome/core/exceptions/connection_exception.dart';
 import 'package:swesshome/core/exceptions/fields_exception.dart';
 import 'package:swesshome/core/exceptions/general_exception.dart';
+import 'package:swesshome/core/exceptions/unauthorized_exception.dart';
 import 'package:swesshome/modules/business_logic_components/bloc/forget_password_bloc/forget_password_event.dart';
 import 'package:swesshome/modules/business_logic_components/bloc/forget_password_bloc/forget_password_state.dart';
-import 'package:swesshome/modules/data/models/user.dart';
 import 'package:swesshome/modules/data/repositories/user_authentication_repository.dart';
 
 class ForgetPasswordBloc extends Bloc<ForgetPasswordEvent, ForgetPasswordState> {
   UserAuthenticationRepository? userAuthenticationRepository = UserAuthenticationRepository();
-
-  User? user;
+  // User? user;
 
   ForgetPasswordBloc({this.userAuthenticationRepository}) : super(ForgetPasswordNone()) {
     on<ForgetPasswordStarted>((event, emit) async {
       emit(ForgetPasswordProgress());
       try {
-        user = await userAuthenticationRepository!.forgetPassword(event.mobile);
+        final message = await userAuthenticationRepository!.forgetPassword(event.mobile);
         // if (user!.token != null) {
         //   UserSharedPreferences.setAccessToken(user!.token!);
         // }
-        emit(ForgetPasswordComplete());
+        emit(ForgetPasswordComplete(successMessage: message));
       } catch (e, stack) {
         if (e is FieldsException) {
           print(e.jsonErrorFields["message"]);
@@ -30,6 +29,8 @@ class ForgetPasswordBloc extends Bloc<ForgetPasswordEvent, ForgetPasswordState> 
           emit(ForgetPasswordError(errorMessage: e.errorMessage));
         } else if (e is ConnectionException) {
            emit(ForgetPasswordError(errorMessage: e.errorMessage, isConnectionError: true));
+        }else if (e is UnauthorizedException) {
+          emit(ForgetPasswordError(errorMessage: e.message, isUnauthorizedError: true),);
         }
         print(e);
         print(stack);
