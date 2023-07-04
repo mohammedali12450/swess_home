17bb7d785113ae59b2d54ecb910742f818d18088
Skaf-Maya <maya98skaf@gmail.com>
2023-07-04 13:24:45 +0300

    adding the success message

--- lib/modules/business_logic_components/bloc/forget_password_bloc/forget_password_state.dart
+++ lib/modules/business_logic_components/bloc/forget_password_bloc/forget_password_state.dart
@@ -4,13 +4,17 @@ class ForgetPasswordNone extends ForgetPasswordState {}
 
 class ForgetPasswordError extends ForgetPasswordState {
   final String? errorMessage;
-
   Map<String, dynamic>? errorResponse;
   final bool isConnectionError;
+  final bool isUnauthorizedError;
 
-  ForgetPasswordError({this.errorMessage, this.errorResponse, this.isConnectionError = false});
+  ForgetPasswordError({this.errorMessage, this.errorResponse, this.isConnectionError = false,this.isUnauthorizedError = false,});
 }
 
 class ForgetPasswordProgress extends ForgetPasswordState {}
 
-class ForgetPasswordComplete extends ForgetPasswordState {}
+class ForgetPasswordComplete extends ForgetPasswordState {
+  final String? successMessage;
+  ForgetPasswordComplete({this.successMessage});
+
+}
