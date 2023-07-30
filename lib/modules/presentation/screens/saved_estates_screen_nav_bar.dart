f5377bb4933ab08b9c3e7abd26f1b6e1776a224d
naeemkashout <naeemkashout@gmail.com>
2023-07-31 01:19:56 +0300

    Delete suggested properties from the saved properties page

--- lib/modules/presentation/screens/saved_estates_screen_nav_bar.dart
+++ lib/modules/presentation/screens/saved_estates_screen_nav_bar.dart
@@ -33,13 +33,14 @@ class SavedEstatesScreenNavBar extends StatefulWidget {
   const SavedEstatesScreenNavBar({Key? key}) : super(key: key);
 
   @override
-  _SavedEstatesScreenNavBarState createState() => _SavedEstatesScreenNavBarState();
+  _SavedEstatesScreenNavBarState createState() =>
+      _SavedEstatesScreenNavBarState();
 }
 
 class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
   late SavedEstatesBloc _savedEstatesBloc;
   List<Estate> estates = [];
-  late bool isArabic,isDark;
+  late bool isArabic, isDark;
   bool? profile;
 
   @override
@@ -58,207 +59,216 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
   Widget build(BuildContext context) {
     isArabic = Provider.of<LocaleProvider>(context).isArabic();
     isDark = Provider.of<ThemeProvider>(context).isDarkMode(context);
-    return BackHomeScreen(child: SafeArea(
-      child: Scaffold(
-        drawer: SizedBox(
-          width: getScreenWidth(context) * (75 / 100),
-          child: const Drawer(
-            child: MyDrawer(),
+    return BackHomeScreen(
+      child: SafeArea(
+        child: Scaffold(
+          drawer: SizedBox(
+            width: getScreenWidth(context) * (75 / 100),
+            child: const Drawer(
+              child: MyDrawer(),
+            ),
           ),
-        ),
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: isDark ? Colors.white : AppColors.black),
-          centerTitle: true,
-          backgroundColor: isDark ? const Color(0xff26282B) : AppColors.white,
-          title: Text(
-            AppLocalizations.of(context)!.saved_estates,
-            style: TextStyle(color: isDark ? Colors.white : AppColors.black),
-          ),
-          actions: [
-            InkWell(
-              child: BlocBuilder<NotificationsCubit, int>(
-                builder: (_, notificationsCount) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        left: isArabic ? 12.w : 0, right: isArabic ? 0 : 12.w),
-                    child: IconBadge(
-                      icon: Icon(
-                          Icons.notifications_outlined,
-                          color: isDark ? Colors.white : AppColors.black
+          appBar: AppBar(
+            iconTheme:
+                IconThemeData(color: isDark ? Colors.white : AppColors.black),
+            centerTitle: true,
+            backgroundColor: isDark ? const Color(0xff26282B) : AppColors.white,
+            title: Text(
+              AppLocalizations.of(context)!.saved_estates,
+              style: TextStyle(color: isDark ? Colors.white : AppColors.black),
+            ),
+            actions: [
+              InkWell(
+                child: BlocBuilder<NotificationsCubit, int>(
+                  builder: (_, notificationsCount) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                          left: isArabic ? 12.w : 0,
+                          right: isArabic ? 0 : 12.w),
+                      child: IconBadge(
+                        icon: Icon(Icons.notifications_outlined,
+                            color: isDark ? Colors.white : AppColors.black),
+                        itemCount: notificationsCount,
+                        right: 0,
+                        top: 5.h,
+                        hideZero: true,
                       ),
-                      itemCount: notificationsCount,
-                      right: 0,
-                      top: 5.h,
-                      hideZero: true,
-                    ),
-                  );
+                    );
+                  },
+                ),
+                onTap: () async {
+                  if (UserSharedPreferences.getAccessToken() == null) {
+                    await showWonderfulAlertDialog(
+                        context,
+                        AppLocalizations.of(context)!.confirmation,
+                        AppLocalizations.of(context)!
+                            .this_features_require_login,
+                        removeDefaultButton: true,
+                        dialogButtons: [
+                          ElevatedButton(
+                            child: Text(
+                              AppLocalizations.of(context)!.sign_in,
+                            ),
+                            onPressed: () async {
+                              await Navigator.pushNamed(
+                                  context, AuthenticationScreen.id);
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ElevatedButton(
+                            child: Text(
+                              AppLocalizations.of(context)!.cancel,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                        width: 400.w);
+                    return;
+                  }
+                  Navigator.pushNamed(context, NotificationScreen.id);
                 },
               ),
-              onTap: () async {
-                if (UserSharedPreferences.getAccessToken() == null) {
-                  await showWonderfulAlertDialog(
-                      context,
-                      AppLocalizations.of(context)!.confirmation,
-                      AppLocalizations.of(context)!.this_features_require_login,
-                      removeDefaultButton: true,
-                      dialogButtons: [
-                        ElevatedButton(
-                          child: Text(
-                            AppLocalizations.of(context)!.sign_in,
-                          ),
-                          onPressed: () async {
-                            await Navigator.pushNamed(
-                                context, AuthenticationScreen.id);
-                            Navigator.pop(context);
-                          },
-                        ),
-                        ElevatedButton(
-                          child: Text(
-                            AppLocalizations.of(context)!.cancel,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                      width: 400.w);
-                  return;
-                }
-                Navigator.pushNamed(context, NotificationScreen.id);
-              },
-            ),
-          ],
-        ),
-        body: RefreshIndicator(
-          color: Theme.of(context).colorScheme.primary,
-          onRefresh: () async {
-            if (UserSharedPreferences.getAccessToken() != null) {
-              _savedEstatesBloc.add(
-                SavedEstatesFetchStarted(
-                    token: UserSharedPreferences.getAccessToken()!),
-              );
-            }
-          },
-          child: SingleChildScrollView(
-            physics: const AlwaysScrollableScrollPhysics(),
-            child: Container(
-              width: 1.sw,
-              height: 1.sh - 75.h,
-              padding: kMediumSymHeight,
-              child: BlocConsumer<SavedEstatesBloc, SavedEstatesState>(
-                bloc: _savedEstatesBloc,
-                listener: (_, savedEstatesState) async {
-                  if (savedEstatesState is SavedEstatesFetchError) {
-                    var error = savedEstatesState.isConnectionError
-                        ? AppLocalizations.of(context)!.no_internet_connection
-                        : savedEstatesState.error;
-                    await showWonderfulAlertDialog(
-                        context, AppLocalizations.of(context)!.error, error);
-                  }
-                },
-                builder: (BuildContext context, savedEstatesState) {
-                  // if (savedEstatesState is SavedEstatesFetchNone) {
-                  //   return FetchResult(
-                  //       content: AppLocalizations.of(context)!
-                  //           .have_not_saved_estates);
-                  // }
+            ],
+          ),
+          body: RefreshIndicator(
+            color: Theme.of(context).colorScheme.primary,
+            onRefresh: () async {
+              if (UserSharedPreferences.getAccessToken() != null) {
+                _savedEstatesBloc.add(
+                  SavedEstatesFetchStarted(
+                      token: UserSharedPreferences.getAccessToken()!),
+                );
+              }
+            },
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Container(
+                width: 1.sw,
+                height: 1.sh - 75.h,
+                padding: kMediumSymHeight,
+                child: BlocConsumer<SavedEstatesBloc, SavedEstatesState>(
+                  bloc: _savedEstatesBloc,
+                  listener: (_, savedEstatesState) async {
+                    if (savedEstatesState is SavedEstatesFetchError) {
+                      var error = savedEstatesState.isConnectionError
+                          ? AppLocalizations.of(context)!.no_internet_connection
+                          : savedEstatesState.error;
+                      await showWonderfulAlertDialog(
+                          context, AppLocalizations.of(context)!.error, error);
+                    }
+                  },
+                  builder: (BuildContext context, savedEstatesState) {
+                    // if (savedEstatesState is SavedEstatesFetchNone) {
+                    //   return FetchResult(
+                    //       content: AppLocalizations.of(context)!
+                    //           .have_not_saved_estates);
+                    // }
 
-                  if (savedEstatesState is SavedEstatesFetchProgress) {
-                    return const PropertyShimmer();
-                  }
-                  if (savedEstatesState is! SavedEstatesFetchComplete) {
-                    return buildSignInRequired(context);
-                    // return FetchResult(
-                    //     content: AppLocalizations.of(context)!
-                    //         .error_happened_when_executing_operation);
-                  }
+                    if (savedEstatesState is SavedEstatesFetchProgress) {
+                      return const PropertyShimmer();
+                    }
+                    if (savedEstatesState is! SavedEstatesFetchComplete) {
+                      return buildSignInRequired(context);
+                      // return FetchResult(
+                      //     content: AppLocalizations.of(context)!
+                      //         .error_happened_when_executing_operation);
+                    }
 
-                  estates = savedEstatesState.savedEstates;
+                    estates = savedEstatesState.savedEstates;
 
-                  if (estates.isEmpty) {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.error_outline,
-                            size: 0.2.sw,
-                            color: Theme.of(context)
-                                .colorScheme
-                                .primary,
-                          ),
-                          kHe24,
-                          Text(
-                            AppLocalizations.of(context)!
-                                .have_not_saved_estates,
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                          kHe24,
-                          Center(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                minimumSize: Size(180.w, 50.h),
-                                maximumSize: Size(200.w, 50.h),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  AppLocalizations.of(context)!.search,
-                                  style: const TextStyle(fontSize: 18),
-                                ),
-                              ),
-                              onPressed: () async {
-                                FocusScope.of(context).unfocus();
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                        const NavigationBarScreen()));
-
-                              },
+                    if (estates.isEmpty) {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.error_outline,
+                              size: 0.2.sw,
+                              color: Theme.of(context).colorScheme.primary,
                             ),
-                          ),
+                            kHe24,
+                            Text(
+                              AppLocalizations.of(context)!
+                                  .have_not_saved_estates,
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                            kHe24,
+                            Center(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(180.w, 50.h),
+                                  maximumSize: Size(200.w, 50.h),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    AppLocalizations.of(context)!.search,
+                                    style: const TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  FocusScope.of(context).unfocus();
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const NavigationBarScreen()));
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          buildSavedList(),
+                          kHe50,
                         ],
                       ),
                     );
-                  }
-                  return SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        // kHe12,
-                        buildSavedList(),
-                        kHe20,
-                        Padding(
-                          padding: kTinyAllPadding,
-                          child: Container(
-                            alignment: Alignment.center,
-                            height: 50.h,
-                            width: 1.sw,
-                            decoration: BoxDecoration(
-                                borderRadius: lowBorderRadius,
-                                // border: Border.all(color: AppColors.primaryColor),
-                                color: isDark ? AppColors.lightblue : AppColors.primaryColor
-                            ),
-                            child: ResText(
-                              AppLocalizations.of(context)!.nearby,
-                              textStyle: Theme.of(context)
-                                  .textTheme
-                                  .headline4!
-                                  .copyWith(fontWeight: FontWeight.w700,color: Colors.white),
-                            ),
-                          ),
-                        ),
-                        buildSavedList(),
-                        kHe50,
-                      ],
-                    ),
-                  );
-                },
+                    // return Column(
+                    //   children: [
+                    // kHe12,
+                    //     buildSavedList(),
+                    //kHe20,
+                    // Padding(
+                    //   padding: kTinyAllPadding,
+                    //   child: Container(
+                    //     alignment: Alignment.center,
+                    //     height: 50.h,
+                    //     width: 1.sw,
+                    //     decoration: BoxDecoration(
+                    //         borderRadius: lowBorderRadius,
+                    //         // border: Border.all(color: AppColors.primaryColor),
+                    //         color: isDark
+                    //             ? AppColors.lightblue
+                    //             : AppColors.primaryColor),
+                    //     child: ResText(
+                    //       AppLocalizations.of(context)!.nearby,
+                    //       textStyle: Theme.of(context)
+                    //           .textTheme
+                    //           .headline4!
+                    //           .copyWith(
+                    //               fontWeight: FontWeight.w700,
+                    //               color: Colors.white),
+                    //     ),
+                    //   ),
+                    // ),
+                    // buildSavedList(),
+                    // kHe50,
+                    //   ],
+                    // );
+                  },
+                ),
               ),
             ),
           ),
         ),
       ),
-    ),);
+    );
   }
 
   Widget buildSavedList() {
@@ -274,6 +284,7 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
       },
     );
   }
+
   Widget buildSignInRequired(context) {
     return SizedBox(
       height: 20,
@@ -295,8 +306,8 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
                   AppLocalizations.of(context)!.sign_in,
                 ),
                 onPressed: () async {
-                  await Navigator.pushNamed(context, AuthenticationScreen.id).then((value) {
-                  });
+                  await Navigator.pushNamed(context, AuthenticationScreen.id)
+                      .then((value) {});
                   Navigator.pop(context);
                   // _onRefresh();
                   if (UserSharedPreferences.getAccessToken() != null) {
@@ -311,6 +322,6 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
           ],
         ),
       ),
-    ) ;
+    );
   }
 }
