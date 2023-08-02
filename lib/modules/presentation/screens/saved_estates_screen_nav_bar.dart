3b1febc1c4163c9a867deb61a6996f32ad4f6621
naeemkashout <naeemkashout@gmail.com>
2023-08-02 10:41:04 +0300

    View suggested real estate on the Saved real estate page

--- lib/modules/presentation/screens/saved_estates_screen_nav_bar.dart
+++ lib/modules/presentation/screens/saved_estates_screen_nav_bar.dart
@@ -4,6 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:swesshome/constants/design_constants.dart';
 import 'package:swesshome/core/functions/screen_informations.dart';
+import 'package:swesshome/modules/business_logic_components/bloc/estate_view_bloc/estate_view_bloc.dart';
+import 'package:swesshome/modules/business_logic_components/bloc/estate_view_bloc/estate_view_event.dart';
+import 'package:swesshome/modules/business_logic_components/bloc/estate_view_bloc/estate_view_state.dart';
 import 'package:swesshome/modules/business_logic_components/bloc/saved_estates_bloc/saved_estates_bloc.dart';
 import 'package:swesshome/modules/business_logic_components/bloc/saved_estates_bloc/saved_estates_event.dart';
 import 'package:swesshome/modules/business_logic_components/bloc/saved_estates_bloc/saved_estates_state.dart';
@@ -39,7 +42,9 @@ class SavedEstatesScreenNavBar extends StatefulWidget {
 
 class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
   late SavedEstatesBloc _savedEstatesBloc;
+  late EstateViewBloc estateViewBloc;
   List<Estate> estates = [];
+  List<Estate> mostViewEstates = [];
   late bool isArabic, isDark;
   bool? profile;
 
@@ -47,10 +52,14 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
   void initState() {
     super.initState();
     _savedEstatesBloc = SavedEstatesBloc(EstateRepository());
+    estateViewBloc = EstateViewBloc(EstateRepository());
     if (UserSharedPreferences.getAccessToken() != null) {
       _savedEstatesBloc.add(
         SavedEstatesFetchStarted(
             token: UserSharedPreferences.getAccessToken()!),
+      );
+      estateViewBloc.add(
+        MostViewEstatesFetchStarted(),
       );
     }
   }
@@ -221,46 +230,51 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
                         ),
                       );
                     }
+                    // return SingleChildScrollView(
+                    //   child: Column(
+                    //     children: [
+                    //       buildSavedList(),
+                    //       kHe50,
+                    //     ],
+                    //   ),
+                    // );
                     return SingleChildScrollView(
                       child: Column(
                         children: [
+                          kHe12,
                           buildSavedList(),
+                          // kHe20,
+                          Padding(
+                            padding: kTinyAllPadding,
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 50.h,
+                              width: 1.sw,
+                              decoration: BoxDecoration(
+                                  borderRadius: lowBorderRadius,
+                                  // border: Border.all(color: AppColors.primaryColor),
+                                  color: isDark
+                                      ? AppColors.lightblue
+                                      : AppColors.primaryColor),
+                              child: ResText(
+                                AppLocalizations.of(context)!.nearby,
+                                textStyle: Theme.of(context)
+                                    .textTheme
+                                    .headline4!
+                                    .copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          buildViewMostList(),
                           kHe50,
                         ],
                       ),
                     );
-                    // return Column(
-                    //   children: [
-                    // kHe12,
-                    //     buildSavedList(),
-                    //kHe20,
-                    // Padding(
-                    //   padding: kTinyAllPadding,
-                    //   child: Container(
-                    //     alignment: Alignment.center,
-                    //     height: 50.h,
-                    //     width: 1.sw,
-                    //     decoration: BoxDecoration(
-                    //         borderRadius: lowBorderRadius,
-                    //         // border: Border.all(color: AppColors.primaryColor),
-                    //         color: isDark
-                    //             ? AppColors.lightblue
-                    //             : AppColors.primaryColor),
-                    //     child: ResText(
-                    //       AppLocalizations.of(context)!.nearby,
-                    //       textStyle: Theme.of(context)
-                    //           .textTheme
-                    //           .headline4!
-                    //           .copyWith(
-                    //               fontWeight: FontWeight.w700,
-                    //               color: Colors.white),
-                    //     ),
-                    //   ),
-                    // ),
-                    // buildSavedList(),
-                    // kHe50,
-                    //   ],
-                    // );
                   },
                 ),
               ),
@@ -268,6 +282,41 @@ class _SavedEstatesScreenNavBarState extends State<SavedEstatesScreenNavBar> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildViewMostList() {
+    return BlocBuilder<EstateViewBloc, EstateViewState>(
+      bloc: estateViewBloc,
+      builder: (context, state) {
+        print('ooooooooooooooooo');
+        print(state);
+        if (state is EstateMostViewFetchProgress) {
+          const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is EstateMostViewFetchComplete) {
+          mostViewEstates = state.estates;
+          return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: mostViewEstates.length,
+            itemBuilder: (_, index) {
+              return EstateHorizonCard(
+                estate: mostViewEstates.elementAt(index),
+                closeButton: false,
+              );
+            },
+          );
+        } else if (state is EstateFetchError) {
+          Center(
+            child: Text('Erorrrrrrrrrrrrrrrrrrrr'),
+          );
+        }
+
+        return Container();
+      },
     );
   }
 
